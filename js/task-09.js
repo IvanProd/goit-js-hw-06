3628fe87d2d2af511e00ebfedd39aca8cb3a1a83
Ivan Prodeus <learning.webpractice@gmail.com>
2022-08-08 23:25:25 +0300

    finished task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+//  console.log();
+const refs= {
+  colorChangeButton: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+  colorSpan: document.querySelector('.color'),
+}
+
+refs.colorChangeButton.addEventListener('click', colorChangeHendle);
+
+function colorChangeHendle(event){
+  refs.body.setAttribute('style', `background-color: ${getRandomHexColor()}`);
+  refs.colorSpan.textContent = `- ${getRandomHexColor()}`;
+  refs.colorSpan.value = getRandomHexColor();
+
+};
+
+
