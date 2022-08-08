929254121adc3f1b2ed98dc8aad256d08712af93
Ivan Prodeus <learning.webpractice@gmail.com>
2022-08-08 19:03:37 +0300

    finishing task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', formSubmitHandle);
+    
+function formSubmitHandle(event){
+    event.preventDefault();
+    const emailValidInput = document.querySelector('[name="email"]').value;
+    const passwordValidInput = document.querySelector('[name="password"]').value;
+
+    if (emailValidInput === '' || passwordValidInput === ''){
+        alert('All fields must be fielled')
+        //console.log(typeof passwordValidInput === '');
+        //console.log(typeof emailValidInput === '');
+        
+    } else{
+        const formData = new FormData(event.currentTarget);
+        formData.forEach((value, name) => {
+        console.log(name,':',value );
+        formEl.reset();
+        });
+    }
+    
+};
